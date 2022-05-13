e9c9915f8d26f2d0c4760c46298e71c998493301
Matthew Pryor <matthewapryor@gmail.com>
2022-05-12 22:30:50 -0400

    saving

--- __tests__/random.test.js
+++
@@ -1,9 +0,0 @@
-const randomNumber = require('../lib/random.js');
-
-test('gets random number between 1 and 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
-module.exports = function () {
-
-};
